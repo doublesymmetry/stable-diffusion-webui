8361eccf88da2c9efb26872bff458f7baee4e7f6
brkirch <brkirch@users.noreply.github.com>
2023-04-09 20:15:40 -0400

    Set default sub-quad sizes based on token size

--- modules/sd_hijack_optimizations.py
+++ modules/sd_hijack_optimizations.py
@@ -279,7 +279,7 @@ def sub_quad_attention(q, k, v, q_chunk_size=1024, kv_chunk_size=None, kv_chunk_
     qk_matmul_size_bytes = batch_x_heads * bytes_per_token * q_tokens * k_tokens
 
     if chunk_threshold is None:
-        chunk_threshold_bytes = 536870912
+        chunk_threshold_bytes = 268435456 * bytes_per_token
     elif chunk_threshold == 0:
         chunk_threshold_bytes = None
     else:
